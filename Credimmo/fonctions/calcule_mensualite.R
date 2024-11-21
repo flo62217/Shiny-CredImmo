1933465c7eb015d61df2eb97f159fb304eed9f72
Lucas Debauche <lucas.debauche@edu.univ-fcomte.fr>
2024-11-21 15:02:24 +0100

    corps fonction calculmensualite avec florian et antoine

--- Credimmo/fonctions/calcule_mensualite.R
+++ Credimmo/fonctions/calcule_mensualite.R
@@ -1,8 +1,8 @@
 #' Calcule les mensualités 
 #'
 #' @param duree_cred numeric, en années
-#' @param taux_int numeric, 
-#' @param taux_ass numeric, 
+#' @param taux_int numeric, taux annuel 
+#' @param taux_ass numeric, taux annuel 
 #' @param montant_proj numeric, montant du projet
 #' @param montant_apport numeric, montant de l'apport
 #' @param rev_emp_1 vecteur de numeric, revenu mensuel net et nombre de mois
@@ -13,16 +13,21 @@
 #' @export
 #'
 #' @examples
-CalculMensualite <- function(duree_cred,
+CalculeMensualite <- function(duree_cred,
                           taux_int,
                           taux_ass,
                           montant_proj,
                           montant_apport,
                           rev_emp_1,
                           rev_emp_2=0,
-                          montant_frais){
+                          montant_frais=0){
   ##corps de la fonction
-  mensualite <- 
+  nb_mensualite <- duree_cred*12
+  montant_emprunt <- montant_proj - montant_apport
+  cout_assurance <- taux_ass*montant_emprunt
+  mensualite_assurance <- cout_assurance/nb_mensualite
+  mensualite_emprunt <- montant_emprunt*(taux_int/12)/(1-(1+taux_int/12)^(-nb_mensualite))
+  mensualite <- mensualite_assurance + mensualite_emprunt +montant_frais/nb_mensualite
   ##outputs
-  mensalite
+  mensualite
 }
