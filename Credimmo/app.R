4ff89f1f979b7b2c9dde1644f837bf68924413d2
Florian Bucquet <florian.bucquet@gmail.com>
2024-12-10 01:26:17 +0100

    Modification de l'affichage et ajout du logo

--- Credimmo/app.R
+++ Credimmo/app.R
@@ -15,17 +15,36 @@ source("fonctions/calcule_mensualite.R", local = TRUE) # Le chemin choisi est pa
 source("fonctions/CreerTableauAmortissement.R", local = TRUE)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("CUB - Crédit Universitaire Bisontin"),
+  includeCSS("www/style.css"),
+  fluidRow(
+    column(
+      width = 1,  #Prend 1/12 de la page
+      img(src = "images/Logo CUB.png", width = "100px", height = "100px")
+    ),
+    column(
+      width = 11,  #Prend 11/12 restant
+      div(
+        #Titre du document et titre de l'onglet (windowTitle)
+        titlePanel("CUB - Crédit Universitaire Bisontin"),
+        style = "display: inline-block; vertical-align: top; margin-left: 10px;"
+      )
+    )
+  ),
     # Cellule pour que l'utilisateur puisse ajouter le montant de son emprunt (valeur du bien)
+    fluidRow(column(width = 4,
     numericInput(inputId = "montant_proj", label = "Montant de l'emprunt", value = 0),
-    sliderInput("duree_cred", label = "Durée du crédit", min = 5, max = 25, value = 5), # J'ai ajouté les durées légales minimales et maximales d'un crédit immobilier (5-25)
-    sliderInput("taux_int", label = "Taux d'intérêt", min = 0, max = 10, value = 2.5, step = .5),
-    sliderInput("taux_ass", label = "Taux d'assurance", min = 0, max = 5, value = 0, step = .5),
-    numericInput(inputId = "montant_apport", label = "Montant de l'apport", value = 0),
-    numericInput(inputId = "rev_emp_1", label = "Revenu de l'emprunteur principal", value = 0),
-    tableOutput("tableau_amortissement")
+    numericInput(inputId = "montant_apport", label = "Montant de l'apport", value = 0)),
+    
+    
+    
+    column(width = 4,
+      sliderInput("duree_cred", label = "Durée du crédit", min = 5, max = 25, value = 5), # J'ai ajouté les durées légales minimales et maximales d'un crédit immobilier (5-25)
+      sliderInput("taux_int", label = "Taux d'intérêt", min = 0, max = 10, value = 2.5, step = .5),
+      sliderInput("taux_ass", label = "Taux d'assurance", min = 0, max = 5, value = 0, step = .5)),
+    column(width = 4,
+           "",
+           numericInput(inputId = "rev_emp_1", label = "Revenu de l'emprunteur principal", value = 0))),
+    fluidRow(column(width = 12, offset = 3,tableOutput("tableau_amortissement")))
 )
 
 # Define server logic required to draw a histogram
