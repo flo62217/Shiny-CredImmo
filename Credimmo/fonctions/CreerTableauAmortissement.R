948da16513b05639b8eb77475cd2e14b7fe80bef
Lucas Debauche <lucas.debauche@edu.univ-fcomte.fr>
2024-12-11 10:30:44 +0100

    fonction CoutTotal et ajout d'un encart associé

--- Credimmo/fonctions/CreerTableauAmortissement.R
+++ Credimmo/fonctions/CreerTableauAmortissement.R
@@ -27,8 +27,8 @@ CreerTableauAmortissement<- function(duree_cred,
   montant_emprunt <- montant_proj - montant_apport
   mois <- seq(1,nb_mois)
   ones <- rep(1,nb_mois)
-  assurance=rep(taux_ass*montant_emprunt,nb_mois)/nb_mois
-  mensualite<-rep(CalculeMensualite(duree_cred,
+  assurance <- rep(taux_ass*montant_emprunt,nb_mois)/nb_mois
+  mensualite <- rep(CalculeMensualite(duree_cred,
                                     taux_int,
                                     taux_ass,
                                     montant_proj,
@@ -36,7 +36,7 @@ CreerTableauAmortissement<- function(duree_cred,
                                     rev_emp_1,
                                     rev_emp_2=0,
                                     montant_frais),nb_mois)
-  restant_du_avec_interet<-(nb_mois-mois+ones)*mensualite
+  restant_du_avec_interet <- (nb_mois-mois+ones)*mensualite
   interet <- rep(0,nb_mois)
   capital_restant_du <- rep(montant_emprunt,nb_mois)
   for(i in 1:nb_mois){
@@ -45,7 +45,7 @@ CreerTableauAmortissement<- function(duree_cred,
   }
   capital_restant_du <- capital_restant_du[1:nb_mois]
   amortissement <- mensualite-interet-assurance
-  #interet<-(restant_du-assurance-montant_frais/nb_mois)*taux_int/12
+  #interet <- (restant_du-assurance-montant_frais/nb_mois)*taux_int/12
   
   ##sortie
   data.frame(mois,restant_du_avec_interet,capital_restant_du,amortissement,interet,assurance,mensualite)
